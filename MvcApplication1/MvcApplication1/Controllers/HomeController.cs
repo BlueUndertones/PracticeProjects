f28bfcb2e8209abe71fc97bef458ebe32b51e964
hgrewette <grewette.heather@gmail.com>
2014-08-08 12:48:33 -0400

    Adding the actual files for mvc by pasting the files into the mvc folder

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = " ";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is edited.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact changed to me.";
+
+            return View();
+        }
+
+
+        public ActionResult Person()
+        {
+            ViewBag.Message = "This is the person page.";
+
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            ViewBag.Message = "Form to take information.";
+
+            return View();
+        }
+
+        public ActionResult ThisIsForTesting()
+        {
+            ViewBag.Message = "Hidden tesing page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ThisIsForTesting(string firstname, string lastname, string email)
+        {
+            ViewData["firstname"] = firstname;
+            ViewData["lastname"] = lastname;
+            ViewData["email"] = email;
+
+            return View();
+        }
+    }
+}
---
+++ MvcApplication1/MvcApplication1/Models/Form.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Form
+    {
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string feedback { get; set; }
+    }
+}
